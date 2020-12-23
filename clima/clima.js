72dc7bea4c9d393862ae77fcfcbbcb4f675d6c52
Freddy QuispeInfosil <fquispe@smartsolutions.com.pe>
2020-12-23 01:52:23 -0500

    Ocultando KEY

--- clima/clima.js
+++ clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getClima = async (lat, lng) => {
 
-    const appid = '12ba06dc928282372ab84d8214d02efc'
+    const appid = 'APP_ID'
 
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=${ appid }&units=metric`)
 
