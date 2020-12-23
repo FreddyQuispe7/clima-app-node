53dcaafc68480e56606bc45d740ded0a51e181e9
Freddy QuispeInfosil <fquispe@smartsolutions.com.pe>
2020-12-23 01:50:39 -0500

    Segundo Commit

--- clima/clima.js
+++ clima/clima.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 
 const getClima = async (lat, lng) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=12ba06dc928282372ab84d8214d02efc&units=metric`)
+    const appid = '12ba06dc928282372ab84d8214d02efc'
+
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=${ appid }&units=metric`)
 
     return resp.data.main.temp;
 }
